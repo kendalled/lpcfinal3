c20dfc36f47b3550187ae7496256211f7a5b2d09
Kendall Jackson <kendallkj00@gmail.com>
2019-05-06 12:55:10 -0400

    Many sitewide tweaks, created 'testimonial' component

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,6 +110,7 @@ $.fn._slider = function(options) {
 
 				// Slide.
 					$slide
+						.css("background-size", "contain")
 						.css('background-image', 'url("' + $img.attr('src') + '")')
 						.css('background-position', ($slide.data('position') ? $slide.data('position') : 'center'));
 
